ea051fc892030ba36e7afc6942cf2c9f15a2959f
Giancarlo Rosa <giannesdabrun@hotmail.com>
2023-05-02 20:10:01 -0300

    Fixed all lint problems.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import PropTypes from 'react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
+
+App.defaultProps = {
+  pageProps: null,
+};
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  // TODO: Check if there is an alternative for this propType.
+  // eslint-disable-next-line react/forbid-prop-types
+  pageProps: PropTypes.object,
+};
