620208a75dc30d3ae4189417bfb6ad8693a36141
Giancarlo Rosa <giannesdabrun@hotmail.com>
2023-05-20 13:04:01 -0300

    chore(storybook): define storybook preview order

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   stories: [
-    '../src/stories/**/*.mdx',
-    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/stories/*.mdx',
+    '../src/stories/*.stories.@(js|jsx|ts|tsx)',
     '../src/components/**/*.mdx',
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
     '../src/layout/**/*.mdx',
     '../src/layout/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/stories/pages/*.mdx',
+    '../src/stories/pages/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
